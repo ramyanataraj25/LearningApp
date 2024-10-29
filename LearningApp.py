2b852c4dd11618cf018bb137206e3cc788ac2476
Ramya Nataraj <ramyan25@terpmail.umd.edu>
2024-10-28 22:01:13 -0400

    Added User class

--- LearningApp.py
+++ LearningApp.py
@@ -3,3 +3,9 @@ class Math:
         self.name = name
         self.grade = grade
         self.score = score
+
+class User:
+    def __init__(self, name, grade, reason):
+        self.name = name
+        self.grade = grade
+        self.reason = reason
