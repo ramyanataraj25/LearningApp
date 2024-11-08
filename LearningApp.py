9fd215b8639379844a808b6e4d2551a658962751
Ramya Nataraj <ramyan25@terpmail.umd.edu>
2024-11-07 20:23:41 -0500

    Updated Program

--- LearningApp.py
+++ LearningApp.py
@@ -1,28 +1,14 @@
-class Math:
-    def __init__(self, name, grade, score):
-        self.name = name
-        self.grade = grade
-        self.score = score
-
 class User:
     def __init__(self, name, grade, reason):
         self.name = name
         self.grade = grade
         self.reason = reason
-        
-class Vocab:
-    def __init__(self, grade, name):
-        self.grade = grade
-        self.name = name
 
-class Summary:
-    def __init__(self, math, vocab, grammar):
-            self.math = math
-            self.vocab = vocab
-            self.grammar = grammar
+
+# Harshitha's Math Class
+        
+# Tatyana's Vocab Class
+
+# Nivitha's Summary Class
  
-class Grammar:
-    def __init__(self, name, grade, feedback):
-	self.name = name
-	self.grade = grade
-	self.feedback = feedback
+# Jess's Grammer Class
